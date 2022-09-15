b16d4bf823851a85c664d798129083f78c7851cb
seanfahey1 <seanfahey21@gmail.com>
2022-09-14 21:31:06 -0700

    fixed batting average calculation

--- SQLProject/baseball-assignment.sql
+++ SQLProject/baseball-assignment.sql
@@ -1,39 +1,44 @@
 -- setting up a player name view b/c what good is any of this if we don't know the player's we're looking at
 create or replace view batter_names as
-select batter, SUBSTRING_INDEX(des, ' ', 2) as name from atbat_r group by batter;
+	select 
+		batter, 
+		SUBSTRING_INDEX(des, ' ', 2) as name 
+	from atbat_r 
+	group by batter
+;
 
 -- career batting average calculion
 select 
-	bc.batter, 
+	bc.batter as id, 
 	bn.name, 
-	bc.team_id, 
+	bc.team_id as team, 
 	sum(bc.atBat) as AB, 
 	sum(bc.Hit) as hits, 
 	sum(bc.Walk) as walks, 
 	sum(bc.Home_Run) as HR, 
-	sum(bc.Hit) / (sum(bc.atBat) - bc.Walk) as bavg 
+	sum(bc.Hit) / sum(bc.atBat) as bavg 
 from batter_counts bc 
 left join batter_names bn on bc.batter=bn.batter 
 group by bc.batter 
 order by hits desc 
-limit 100 ;
-
+;
 
 
 -- batting average calc per year
 select 
-	bc.batter, 
+	bc.batter as id, 
 	bn.name, 
-	bc.team_id, 
+	bc.team_id as team, 
 	sum(bc.atBat) as AB, 
 	sum(bc.Hit) as hits, 
 	sum(bc.Walk) as walks, 
 	sum(bc.Home_Run) as HR, 
-	sum(bc.Hit) / (sum(bc.atBat) - bc.Walk) as bavg, 
+	sum(bc.Hit) / sum(bc.atBat) as bavg, 
 	substring_index(g.local_date, "-", 1) as year
 from batter_counts bc 
 left join game g on g.game_id = bc.game_id
 left join batter_names bn on bc.batter=bn.batter 
 where substring_index(g.local_date, "-", 1) is not null
-group by batter, year
+group by bc.batter, year
+order by hits desc
 ;
