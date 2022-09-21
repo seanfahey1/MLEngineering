6a66f95ca9e57948c2f4baa9481906c109674001
seanfahey1 <seanfahey21@gmail.com>
2022-09-20 19:43:12 -0700

    small cleanups based on in-class suggestions

--- SQLProject/baseball-assignment.sql
+++ SQLProject/baseball-assignment.sql
@@ -1,5 +1,5 @@
 -- startup/cleanup tasks
-drop view if exists batter_names;
+drop view if exists batter_name;
 drop table if exists career_bavg;
 drop table if exists year_bavg;
 drop table if exists per_game_stats;
@@ -7,7 +7,7 @@ drop table if exists rolling_bavg;
 
 
 -- setting up a player name view b/c what good is any of this if we don't know the names of player's that we're looking at
-create or replace view batter_names as
+create or replace view batter_name as
 	select 
 		batter, 
 		SUBSTRING_INDEX(des, ' ', 2) as name 
@@ -23,15 +23,15 @@ create or replace table career_bavg
 		bc.team_id as teamid, 
 		t.name_brief as team,
 		sum(bc.atBat) as AB, 
-		sum(bc.Hit) as hits, 
-		sum(bc.Walk) as walks, 
+		sum(bc.Hit) as hit, 
+		sum(bc.Walk) as walk, 
 		sum(bc.Home_Run) as HR, 
 		case when sum(bc.atBat) = 0 then 0 else sum(bc.Hit) / sum(bc.atBat) end as bavg
 	from batter_counts bc 
-	left join batter_names bn on bc.batter=bn.batter
+	left join batter_name bn on bc.batter=bn.batter
 	left join team t on t.team_id = bc.team_id
 	group by bc.batter 
-	order by hits desc 
+	order by hit desc 
 ;
 
 
@@ -43,18 +43,18 @@ create or replace table year_bavg
 		bc.team_id as teamid, 
 		t.name_brief as team,
 		sum(bc.atBat) as AB, 
-		sum(bc.Hit) as hits, 
-		sum(bc.Walk) as walks, 
+		sum(bc.Hit) as hit, 
+		sum(bc.Walk) as walk, 
 		sum(bc.Home_Run) as HR, 
 		case when sum(bc.atBat) = 0 then 0 else sum(bc.Hit) / sum(bc.atBat) end as bavg,
 		substring_index(g.local_date, "-", 1) as year
 	from batter_counts bc 
 	left join game g on g.game_id = bc.game_id
-	left join batter_names bn on bc.batter=bn.batter 
+	left join batter_name bn on bc.batter=bn.batter 
 	left join team t on t.team_id = bc.team_id
 	where substring_index(g.local_date, "-", 1) is not null /* remove null dates */
 	group by bc.batter, year
-	order by hits desc
+	order by hit desc
 ;
 
 -- rolling 100 day average: 2 step process
@@ -67,14 +67,14 @@ create or replace table per_game_stats as
 		t.name_brief as team,
 		g.`game_id`as game,
 		bc.atBat as AB, 
-	 	bc.Hit as hits, 
-	 	bc.Walk as walks, 
+	 	bc.Hit as hit, 
+	 	bc.Walk as walk, 
 		bc.Home_Run as HR, 
 		case when bc.atBat = 0 then 0 else bc.Hit / bc.atBat end as bavg,
 		cast(g.local_date as date) as date
 	from batter_counts bc 
 	left join game g on g.game_id = bc.game_id
-	left join batter_names bn on bc.batter=bn.batter
+	left join batter_name bn on bc.batter=bn.batter
 	left join team t on t.team_id = bc.team_id
 	where substring_index(g.local_date, "-", 1) is not null /* remove null dates */ and bn.name is not null
 	group by id, date
@@ -92,8 +92,8 @@ create or replace table rolling_bavg as
 		/* date diff needs restrictions on both ends of range, plus case restriction to make sure the player id matches. */
 		/* haven't tested what happens when a player switches teams (id id changes or not) */
 		sum(case when datediff(p1.date, p2.date) between 0 and 100 and p1.id = p2.id then p2.AB end) as 100_day_ab,
-		sum(case when datediff(p1.date, p2.date) between 0 and 100 and p1.id = p2.id then p2.hits end) as 100_day_hits,
-		sum(case when datediff(p1.date, p2.date) between 0 and 100 and p1.id = p2.id then p2.walks end) as 100_day_walks,
+		sum(case when datediff(p1.date, p2.date) between 0 and 100 and p1.id = p2.id then p2.hit end) as 100_day_hit,
+		sum(case when datediff(p1.date, p2.date) between 0 and 100 and p1.id = p2.id then p2.walk end) as 100_day_walk,
 		sum(case when datediff(p1.date, p2.date) between 0 and 100 and p1.id = p2.id then p2.HR end) as 100_day_HR,
 		avg(case when datediff(p1.date, p2.date) between 0 and 100 and p1.id = p2.id then p2.bavg end) as 100_day_bavg,
 		sum(case when datediff(p1.date, p2.date) between 0 and 100 and p1.id = p2.id then 1 end) as num_games
