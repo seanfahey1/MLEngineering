5e283c82b516b01498357d3fc1d5658960f74c7f
Sean Fahey <sean@Pro.lan>
2022-10-10 22:06:10 -0700

    linear regression working

--- FeatureEngineering/feature-engineering.py
+++ FeatureEngineering/feature-engineering.py
@@ -1,7 +1,9 @@
+import random
 import sys
 
 import pandas as pd
 import plotly.express as px
+import statsmodels.api
 
 debug = True
 
@@ -24,11 +26,14 @@ def inspect_cont_pred_cont_resp(df, predictor, response):
     """
     if debug:
         return
+    p_value, t_value = do_regression(df, predictor, response, method="linear")
+
     fig = px.scatter(
         x=df[predictor],
         y=df[response],
         trendline="ols",
-        title=f"<b>{predictor} v. {response}<b/><br>continuous v. continuous",
+        title=f"<b>{predictor} v. {response}</b><br>continuous v. continuous<br>"
+        f"<i>p-value: {p_value}, t-value: {t_value}</i>",
     )
     fig.update_layout(
         xaxis_title=predictor,
@@ -37,23 +42,36 @@ def inspect_cont_pred_cont_resp(df, predictor, response):
     fig.show()
 
 
-def inspect_bool_pred_cont_resp(df, predictor, response):
+def inspect_bool_pred_cont_resp(df, predictor, response, reverse=False):
     """
     Generates violin plot grouped by the predictor when the predictor is CATEGORICAL/BOOLEAN and the response is
-    CONTINUOUS.
+    CONTINUOUS. This function also works for the reverse (switching predictor and response) when reverse is set to
+    True.
     """
     if debug:
-        return
+        pass
+
+    if reverse:
+        cat = response
+        cont = predictor
+        # p_value, t_value = do_regression(df, predictor, response, method="logistic-reversed")
+        title_text = "continuous v. categorical"
+    else:
+        cat = predictor
+        cont = response
+        # p_value, t_value = do_regression(df, predictor, response, method="logistic")
+        title_text = "categorical v. continuous"
+
     fig = px.violin(
-        color=df[predictor],
-        y=df[response],
+        color=df[cat],
+        y=df[cont],
         points="all",
         box=True,
-        title=f"<b>{predictor} v. {response}</b><br>categorical v. continuous",
+        title=f"<b>{cat} v. {cont}</b><br>{title_text}",
     )
     fig.update_layout(
-        xaxis_title=predictor,
-        yaxis_title=response,
+        xaxis_title=cat,
+        yaxis_title=cont,
     )
     fig.show()
 
@@ -82,22 +100,71 @@ def inspect_bool_pred_bool_resp(df, predictor, response):
     fig.show()
 
 
-def main():
-    # Using titanic dataset as placeholder for testing. Any pandas dataframe can be inserted here
+def do_regression(df, predictor, response, method):
+    # drop any NaN values
+    print(predictor, response)
+    df = df[[predictor, response]].dropna()
+    print(df)
+
+    # do p and t calculations
+    if method == "linear":
+        pred = statsmodels.api.add_constant(df[predictor])
+        fitted_output = statsmodels.api.OLS(df[response], pred).fit()
+
+    # elif method == "logistic":
+    #     pred = statsmodels.api.add_constant(df[predictor])
+    #     fitted_output = statsmodels.api.Logit(df[predictor], df[response]).fit()
+    #
+    # elif method == "logistic-reversed":
+    #     print('reversed')
+    #     print(df[predictor])
+    #     pred = statsmodels.api.add_constant(df[response].to_numpy())
+    #     fitted_output = statsmodels.api.Logit(df[predictor].to_numpy(), pred).fit()
+
+    else:
+        raise ValueError("Invalid method")
+
+    # Get the stats!
+    t_value = round(fitted_output.tvalues[1], 6)
+    p_value = "{:.6e}".format(fitted_output.pvalues[1])
+    return p_value, t_value
+
+
+def dataset_insert():
+    """
+    Function to easily drop in different csv formatted datasets and lists of predictor and response columns to use.
+    """
     df = pd.read_csv(
         "https://raw.githubusercontent.com/jorisvandenbossche/pandas-tutorial/master/data/titanic.csv"
     )
-    predictors = ["Sex", "Age", "SibSp", "Parch", "Fare", "Pclass", "Embarked"]
+    df["test1"] = [random.choice([True, False]) for i in range(len(df))]
+    df["test2"] = [random.choice([0, 1]) for i in range(len(df))]
+    predictors = ["Sex", "Age", "SibSp", "Parch", "Fare", "Pclass", "test1", "test2"]
     response = "Survived"
-    # response = "Age"
-    response_continuous = determine_continuous(df, response)
+    # response = "Fare"
+    return df, predictors, response
 
+
+def main():
+    # Using titanic dataset as placeholder for testing. Any pandas dataframe can be inserted here
+    df, predictors, response = dataset_insert()
+
+    response_continuous = determine_continuous(df, response)
+    cont_lookup = dict()
+
+    # loop through each predictor and generate the correct plot
     for predictor in predictors:
+        # determine if predictor is categorical or continuous
         predictor_continuous = determine_continuous(df, predictor)
+        cont_lookup[predictor] = predictor_continuous
+
+        # use cat/cont info to determine which plotting function to use
         if response_continuous and predictor_continuous:
             inspect_cont_pred_cont_resp(df, predictor, response)
         elif response_continuous and not predictor_continuous:
             inspect_bool_pred_cont_resp(df, predictor, response)
+        elif not response_continuous and predictor_continuous:
+            inspect_bool_pred_cont_resp(df, predictor, response, reverse=True)
         elif not response_continuous and not predictor_continuous:
             inspect_bool_pred_bool_resp(df, predictor, response)
 
