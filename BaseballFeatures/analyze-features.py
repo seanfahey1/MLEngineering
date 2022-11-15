c4c459ba4a83008bb6de53d72c56477649cd8b77
Sean Fahey <sean@Pro.lan>
2022-11-14 20:55:25 -0800

    imported midterm code

--- BaseballFeatures/analyze-features.py
+++ BaseballFeatures/analyze-features.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
 import sys
+from pathlib import Path
 
+import pandas as pd
 from pyspark import StorageLevel
 from pyspark.ml.feature import SQLTransformer
 from pyspark.sql import SparkSession
 
+HERE = Path(__file__).parent
+sys.path.append(str(HERE / "../midterm"))
+import midterm  # noqa: E402
 
-def main():
+
+def baseballdb_connection(
+    user: str, password: str, jar_path: str, dbtable: str, query: str
+) -> pd.DataFrame:
+
     app_name = "baseball mariadb spark"
     master = "local"
-    jar_path = (
-        "/Users/sean/workspace/Sean/sdsu/BDA602/DBs/mariadb-java-client-3.0.8.jar"
-    )
+    jdbc_url = "jdbc:mysql://localhost:3306/baseballdb?permitMysqlScheme"
+    jdbc_driver = "org.mariadb.jdbc.Driver"
+
+    # initialize spark and set logging level to suppress warnings
     spark = (
         SparkSession.builder.config("spark.jars", jar_path)
         .appName(app_name)
         .master(master)
         .getOrCreate()
     )
+    spark.sparkContext.setLogLevel("FATAL")
 
-    user = "sean"
-    password = input("enter password...\t")  # pragma: allowlist secret
-
-    jdbc_url = "jdbc:mysql://localhost:3306/baseballdb?permitMysqlScheme"
-    jdbc_driver = "org.mariadb.jdbc.Driver"
-    dbtable = "baseballdb.game_features"
-
-    game_features = (
+    table = (
         spark.read.format("jdbc")
         .option("url", jdbc_url)
         .option("driver", jdbc_driver)
@@ -36,16 +40,30 @@ def main():
         .load()
     )
 
-    game_features.createOrReplaceTempView("game_features_pyspark")
-    game_features.persist(StorageLevel.DISK_ONLY)
-
-    query = "select * from game_features_pyspark;"
+    table.createOrReplaceTempView("game_features_pyspark")
+    table.persist(StorageLevel.DISK_ONLY)
 
     df = SQLTransformer().setStatement(query)
-    df.transform(game_features).show()
-    df = df.transform(game_features).toPandas()
-    print(len(df))
-    print(len(df.dropna(axis=0)))
+    # df.transform(game_features).show()
+    df = df.transform(table).select("*").toPandas()
+
+    return df
+
+
+def main():
+    jar_path = (
+        "/Users/sean/workspace/Sean/sdsu/BDA602/DBs/mariadb-java-client-3.0.8.jar"
+    )
+    user = "sean"
+    password = input("enter password...\t")  # pragma: allowlist secret
+    dbtable = "baseballdb.game_features"
+    query = "select * from game_features_pyspark;"
+
+    df = baseballdb_connection(user, password, jar_path, dbtable, query)
+
+    midterm.difference_w_mean_of_resp_1d_cat_pred(
+        df, "away_pitcher_WHIP_100", "home_team_wins", Path(".")
+    )
 
 
 if __name__ == "__main__":
