0666a72cdbd9d5ad22a14dbbc41f3fabb358e611
Sean Fahey <sean@Pro.lan>
2022-11-30 15:48:08 -0800

    small changes

--- Docker/run.sh
+++ Docker/run.sh
@@ -4,10 +4,10 @@ DB=`mysqlshow --user=root --password=fahey -h db| grep -v Wildcard | grep -o bas
 
 if [ "$DB" == "baseballdb" ]; then
     echo 'BaseballDB found'
-    echo 'BaseballDB found:' >> output/connection-check.txt
+    echo 'BaseballDB found:' >> output/log.txt
 else
     echo 'BaseballDB not found'
-    echo 'BaseballDB not found:' >> output/connection-check.txt
+    echo 'BaseballDB not found:' >> output/log.txt
     mysqladmin --user=root --password=fahey -h db create baseballdb
     echo 'Created empty BaseballDB'
     mysql --user=root --password=fahey -h db --database=baseballdb < baseball.sql
@@ -16,5 +16,5 @@ fi
 
 mysql --user=root --password=fahey -h db < 100_day_rolling_calc.sql > output/results.txt
 
-date +"%T" >> output/connection-check.txt
-echo "" >> output/connection-check.txt
+date >> output/log.txt
+echo "" >> output/log.txt
